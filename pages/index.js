79b498414df76679830b003ab6b0d5368fdeeb9c
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-06-19 22:59:00 +0000

    add configuration for eslint

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-export default function Home() {
-  return (
-    <div>
+const Home = () => (
+  <div>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
-    </div>
-  )
-}
+  </div>
+);
+export default Home;
