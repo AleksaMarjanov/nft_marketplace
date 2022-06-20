3066f30a51054147e5dfa542dfa64e272bda840a
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-06-20 18:32:11 -0500

    add footer component & fix bug swc-loader

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -108,7 +108,15 @@ const Navbar = () => {
       <div className="hidden md:flex ml-2">
         {isOpen
           ? (
-            <Image src={images.cross} />
+            <Image
+              src={images.cross}
+              objectFit="contain"
+              width={20}
+              height={20}
+              alt="close"
+              onClick={() => setIsOpen(false)}
+              className={theme === 'light' && 'filter invert'}
+            />
           ) : (
             <Image
               src={images.menu}
@@ -120,6 +128,17 @@ const Navbar = () => {
               className={theme === 'light' && 'filter invert'}
             />
           )}
+        {isOpen && (
+        <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+          <div className="flex-1 p-4">
+            <MenuItems active={active} setActive={setActive} isMobile />
+          </div>
+          <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+            <ButtonGroup setActive={setActive} router={router} />
+          </div>
+
+        </div>
+        )}
       </div>
 
     </nav>
