183d3b3b5ff764211ce39db0b461be5eeea8b8f7
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-06-23 21:06:34 +0000

    create nftmarketplace contract

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarketplace is ERC721UriStorage {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+}
