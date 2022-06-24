652639eda1dca9d4d5b92f2fce6c9c0a2b74c5a1
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-06-24 18:30:38 -0500

    add Context for entire react app/connectingFRONT_BLOCKCHAIN

---
+++ context/NFTContext.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import Web3Modal from 'web3modal';
+import { ethers } from 'ethers';
+import axios from 'axios';
+
+import { MarketAddress, MarketAddressABI } from './constants';
+
+export const NFTContext = React.createContext();
+
+export const NFTProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState('');
+  const nftCurrency = 'ETH';
+
+  const checkIfWalletIsConnected = () => {
+
+  };
+
+  return (
+    <NFTContext.Provider value={{ nftCurrency }}>
+      {children}
+    </NFTContext.Provider>
+  );
+};
