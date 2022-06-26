965fd696814f3976103d5d36d222d4a08258ad28
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-06-26 13:17:20 -0500

    Add Payment Modal on NFTs detail page

--- components/index.js
+++ components/index.js
@@ -6,5 +6,6 @@ import CreatorCard from './CreatorCard';
 import NFTCard from './NFTCard';
 import Input from './Input';
 import Loader from './Loader';
+import Modal from './Modal';
 
-export { Navbar, Footer, CreatorCard, Button, Banner, NFTCard, Input, Loader };
+export { Navbar, Footer, CreatorCard, Button, Banner, NFTCard, Input, Loader, Modal };
--- utils/shortenAddress.js
+++ utils/shortenAddress.js
@@ -1,3 +1,3 @@
 export const shortenAddress = (address) => (
-  `${address.slice(0, 5)}...${address.slice(address.length - 4)}}`
+  `${address.slice(0, 5)}..${address.slice(address.length - 4)}`
 );
