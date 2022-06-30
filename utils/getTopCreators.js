6abd6e1cf8ef3bb1febeebeafa61a620c2702a8c
Aleksa Marjanov <Aleksa.marjanov@outlook.com>
2022-06-30 02:06:44 +0000

    added top sellers functionality

--- utils/getTopCreators.js
+++ utils/getTopCreators.js
@@ -1,13 +1,11 @@
 export const getCreators = (nfts) => {
   const creators = nfts.reduce((creatorObject, nft) => {
-    const creator = creatorObject[nft.seller] || [];
-
-    creator.push(nft);
+    (creatorObject[nft.seller] = creatorObject[nft.seller] || []).push(nft);
 
     return creatorObject;
   }, {});
 
-  Object.entries(creators).map((creator) => {
+  return Object.entries(creators).map((creator) => {
     const seller = creator[0];
     const sum = creator[1].map((item) => Number(item.price)).reduce((prev, curr) => prev + curr, 0);
 
